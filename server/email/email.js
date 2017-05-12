fb293e623c16021cdaefabedbbe859a62958a0c0
oliwer94 <oliwer94@gmail.com>
2017-05-12 15:58:43 +0200

    nodemailer ->  smtpTransport

--- server/email/email.js
+++ server/email/email.js
@@ -1,14 +1,15 @@
 /*jshint esversion: 6 */
 const nodemailer = require('nodemailer');
+const smtpTransport = require('nodemailer-smtp-transport');
 
 // create reusable transporter object using the default SMTP transport
-let transporter = nodemailer.createTransport({
+let transporter = nodemailer.createTransport(smtpTransport({
     service: 'gmail',
     auth: {
         user: process.env.GMAIL_USERNAME,// 'bananbaszo@gmail.com',  noreply.meyespace@gmail.com
         pass: process.env.GMAIL_PASSWORD//'bananbaszo94'
     }
-});
+}));
 module.exports = {
     transporter
 };
