7a1d562ebda3970e3414233c67f792e831dc16ea
oliwer94 <oliwer94@gmail.com>
2017-05-27 02:37:18 +0200

    authorization into cors headers

--- server/server.js
+++ server/server.js
@@ -52,7 +52,7 @@ app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", process.env.CORS);//'https://meyespace-frontend.com'); //<-- you can change this with a specific url like http://localhost:4200
     res.header("Access-Control-Allow-Credentials", "true");
     res.header('Access-Control-Allow-Methods', 'OPTIONS,GET,PUT,POST,DELETE');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    res.header("Access-Control-Allow-Headers", 'Authorization,Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
     next();
 });
 
